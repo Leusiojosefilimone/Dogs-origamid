edac6b665d04dc6d7c48399f720709111f06b4f8
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-02 12:53:39 -0300

    terminei o header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

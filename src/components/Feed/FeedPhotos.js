ef26d8b2357d9147b707d8267591fc69c963aee1
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-16 15:53:54 -0300

    scroll infinito

--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -6,17 +6,21 @@ import Loading from '../helper/Loading';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTOS_GET } from '../../Api';
 
-function FeedPhotos({ setModalPhoto }) {
+function FeedPhotos({
+  page, user, setModalPhoto, setInfinite,
+}) {
   const {
     data, loading, request, error,
   } = useFetch();
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.legth < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;

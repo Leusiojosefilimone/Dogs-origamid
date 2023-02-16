8c017605769548c5abc83685d7d170d6c0956171
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-16 15:19:40 -0300

    logica de paginação

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,12 +2,48 @@ import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
-function Feed() {
+function Feed({ user }) {
   const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
+
+  React.useEffect(() => {
+    function infiniteScroll() {
+      if (infinite) {
+        let wait = false;
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = true;
+          }, 500);
+        }
+      }
+    }
+
+    window.addEventListener('wheel', infiniteScroll);
+    window.addEventListener('wheel', infiniteScroll);
+    return () => {
+      window.removeEventListener('wheel', infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
+    };
+  }, [infinite]);
+
   return (
     <div>
+
+      {pages.map((page) => (
+        <FeedPhotos
+          user={user}
+          key={page}
+          page={page}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
       {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 }

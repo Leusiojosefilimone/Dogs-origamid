25f05dd3d8ba952d7a16560ef4a4f16625cac967
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-17 15:40:21 -0300

    Fetching the photo
    Photo is a main container of the photo

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { PHOTO_GET } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../helper/Error';
+import Loading from '../helper/Loading';
 import styles from './Photo.module.css';
+import PhotoContent from './PhotoContent';
 
-function photo() {
-  return (
-    <div className={styles.photo}>photo</div>
+function Photo() {
+  const single = true;
+  const { id } = useParams();
+  const {
+    data, loading, error, request,
+  } = useFetch();
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if (error) return <Error erro={error} />;
+  if (loading) return <Loading />;
+  if (data) return (
+    <section className="container mainContainer">
+      <PhotoContent single={single} data={data} />
+    </section>
   );
 }
 
-export default photo;
+export default Photo;

f99ab2d855339a88be19da08f0905bc0a68d2b30
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-18 03:55:18 -0300

    Send a email or username to get new password

--- src/components/Login/LoginPasswordLost.js
+++ src/components/Login/LoginPasswordLost.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { PASSWORD_LOST } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
+import useForm from '../../Hooks/useForm';
+import Button from '../form/Button';
+import Input from '../form/Input';
+import Error from '../helper/Error';
 
 function LoginPasswordLost() {
+  const login = useForm();
+  const {
+    data, error, loading, request,
+  } = useFetch();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate) {
+      const { url, options } = PASSWORD_LOST({ login: login.value, url: window.location.href.replace('perdeu', 'resetar') });
+      const { json } = await request(url, options);
+    }
+  }
+
   return (
-    <div>LoginPasswordLost</div>
+    <section>
+      <h1 className="title">Perdeu a senha</h1>
+      {data ? <p style={{ color: '#4c1' }}>{data}</p> : (
+        <from onSubmit={handleSubmit}>
+          <Input type="text" label="Email / Usuário" name="email" {...login} />
+          {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar</Button>}
+        </from>
+      )}
+
+      <Error erro={error} />
+    </section>
   );
 }
 

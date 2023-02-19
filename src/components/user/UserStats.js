0d55f971bd2b2fbceb11f3d79f40a7eb8e5693b1
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-19 03:27:00 -0300

    Fetchs the stats
    afther that a sent the stats to userStatsGraphs

--- src/components/user/UserStats.js
+++ src/components/user/UserStats.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { STATS_GET } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../helper/Loading';
 import Head from '../helper/Head';
+import Error from '../helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 function UserStats() {
-  return (
-    <div>
-      <Head
-        title="Estatisticas"
-        description="Estatistias do usuario"
-      />
-      UserStats
-
-    </div>
-  );
+  const data = [
+    { title: 'peixe', acessos: '12' },
+    { title: 'robin', acessos: '21' },
+    { title: 'cat', acessos: '27' },
+  ];
+  const {
+    error, loading, request,
+  } = useFetch();
+  React.useEffect(() => {
+    async function getdata() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getdata();
+  }, [request]);
+  if (loading) return <Loading />;
+  if (error) return <Error erro={error} />;
+  if (data) {
+    return (
+      <div>
+        <Head
+          title="Estatisticas"
+          description="Estatistias do usuario"
+        />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  } else return null;
 }
 
 export default UserStats;

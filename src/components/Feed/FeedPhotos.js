d9178258c4273e3560a4a5854059b477635ba793
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-19 05:00:39 -0300

    solve  infinite request problems

--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -5,6 +5,7 @@ import Error from '../helper/Error';
 import Loading from '../helper/Loading';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTOS_GET } from '../../Api';
+import NoPhoto from '../helper/NoPhoto';
 
 function FeedPhotos({
   page, user, setModalPhoto, setInfinite,
@@ -13,18 +14,21 @@ function FeedPhotos({
     data, loading, request, error,
   } = useFetch();
   React.useEffect(() => {
+    // eslint-disable-next-line consistent-return
     async function fetchPhotos() {
       const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      if (response && response.ok && json.legth < total) setInfinite(false);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
   }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
+  if (data && data.length === 0) return <NoPhoto />;
   if (data) {
+    console.log(data);
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (

f73d8dd73cf8fad459d75823e3d0915242d5821f
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-19 03:39:10 -0300

    solves a problem with stats route

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -17,7 +17,7 @@ function User() {
       <Routes>
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
-        <Route path="estatistica" element={<UserStats />} />
+        <Route path="estatisticas" element={<UserStats />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
